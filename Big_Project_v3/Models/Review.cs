45068b69f1a4f5aee6804fc51c9e713bdf9cb5e6
tommy6838 <tommy6838@gmail.com>
2024-11-23 21:20:04 +0800

    新增評論的星星與Review的Rating資料型態從int修改為double

--- Big_Project_v3/Models/Review.cs
+++ Big_Project_v3/Models/Review.cs
@@ -45,6 +45,8 @@ public partial class Review
     /// </summary>
     public DateTime? UpdatedAt { get; set; }
 
+    public string IsReviewLocked { get; set; } // 新增欄位
+
     public virtual Restaurant? Restaurant { get; set; }
 
     public virtual User? User { get; set; }
