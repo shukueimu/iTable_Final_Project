3c2113eca39fbe12549cb837244a125dc3faed88
tommy6838 <tommy6838@gmail.com>
2024-11-27 01:01:10 +0800

    修改評論修復完成

--- Big_Project_v3/ViewModels/EditReviewViewModel.cs
+++ Big_Project_v3/ViewModels/EditReviewViewModel.cs
@@ -1,10 +1,15 @@
-﻿namespace Big_Project_v3.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Big_Project_v3.ViewModels
 {
     public class EditReviewViewModel
     {
         public int ReviewID { get; set; }
+
         public int RestaurantID { get; set; }
+
         public double Rating { get; set; }
+
         public string ReviewText { get; set; }
     }
 }
